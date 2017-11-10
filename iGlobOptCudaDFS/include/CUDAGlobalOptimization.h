70e5d029bf1b3254c40097fd6a1e6d8874f5530e
tkacleonid <tkacleonid@ya.ru>
2017-11-10 05:22:29 +0300

    Debug CUDA DFS

--- iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
+++ iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
@@ -435,7 +435,7 @@ __global__ void globOptCUDA(double *inBox, int inRank, int *workLen, double *min
 		
 		bInd = threadId*1024*(2*inRank+3) + (workLen_s[threadId] - 1)*(2*inRank+3);
 		fnCalcFunLimitsRozenbroke_CUDA(inBox + bInd, inRank);
-		/*
+		
 		if(min_s[threadId] > inBox[bInd + 2*inRank + 2])
 		{
 			min_s[threadId] = inBox[bInd + 2*inRank + 2];
