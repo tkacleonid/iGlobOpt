e4b0e8ed5ab1f4fa8a2341d4bd70d828d29f283c
tkacleonid <tkacleonid@ya.ru>
2017-11-10 03:41:30 +0300

    fixing DFS

--- iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
+++ iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
@@ -689,7 +689,7 @@ __global__ void globOptCUDA(double *inBox, int inRank, int *workLen, double *min
 	while(workLen_s[threadId] > 0)
 	{
 		bInd = threadId*1024*(2*inRank+3) + (workLen_s[threadId] - 1)*(2*inRank+3);
-		fnCalcFunLimitsRozenbroke_CUDA(inBox[threadId*1024*(2*inRank+3) + (workLen_s[threadId] - 1)*(2*inRank+3)], inRank);
+		fnCalcFunLimitsRozenbroke_CUDA(inBox[bInd], inRank);
 		if(min_s[threadId] > inBox[bInd + 2*inRank + 2])
 		{
 			min_s[threadId] = inBox[bInd + 2*inRank + 2];
