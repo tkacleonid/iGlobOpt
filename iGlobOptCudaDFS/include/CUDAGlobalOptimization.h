939155536a266b8d85e5fe832a6d50b73e43b881
tkacleonid <tkacleonid@ya.ru>
2017-11-13 14:26:08 +0300

    debug cuda dfs

--- iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
+++ iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
@@ -597,7 +597,7 @@ __global__ void globOptCUDA(double *inBox, int inRank, int *workLen, double *min
 					n = 0;
 					for(i = 0; i < workLen_s[threadId]*2; i++)
 					{
-						if(min_s[threadId] - temp[i*(2*inRank+3) + 2*inRank] < inEps)
+						if(min_s[threadId] - temp[i*(2*inRank+3) + 2*inRank] > inEps)
 						{
 							memcpy(inBox + threadId*1024*(2*inRank+3) + i*(2*inRank+3),temp + i*(2*inRank+3),sizeof(double)*(2*inRank+3));
 							++n;
