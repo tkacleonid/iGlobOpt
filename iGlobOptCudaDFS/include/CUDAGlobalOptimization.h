c137719a8fde8b7d78595d7f3dfb6c3aa4b643a2
tkacleonid <tkacleonid@ya.ru>
2017-11-13 15:31:00 +0300

    debug cuda dfs

--- iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
+++ iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
@@ -428,7 +428,7 @@ void sendDataToCuda_deep(double *inBox, int inRank, int inFunc, int numBoxes, in
 	{
 		for(int j = 0; j < inRank; j++)
 		{
-			std::cout << "[" << inBox[i*1024*2*inRank + 3) + j*2] << "; " << inBox[i*1024*(2*inRank + 3) + j*2 + 1] << "]\t";
+			std::cout << "[" << inBox[i*1024*(2*inRank + 3) + j*2] << "; " << inBox[i*1024*(2*inRank + 3) + j*2 + 1] << "]\t";
 		}
 		std::cout << "\t\t" << inBox[i*1024*(2*inRank + 3) + inRank*2] << "\t\t" << inBox[i*1024*(2*inRank + 3) + inRank*2 + 1] << "\t\t" << inBox[i*1024*(2*inRank + 3) + inRank*2 + 2];
 		std::cout <<  "\n";
