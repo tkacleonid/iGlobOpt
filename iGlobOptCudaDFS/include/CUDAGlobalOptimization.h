5887e4341c28c35c3364a941080507712827a2ae
tkacleonid <tkacleonid@ya.ru>
2017-11-14 23:19:21 +0300

    debug cuda dfs

--- iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
+++ iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
@@ -591,8 +591,8 @@ __global__ void globOptCUDA(double *inBox, const int inRank, int *workLen, doubl
 		if(workLen_s[threadId] > 0)
 		{
 			
-			bInd = threadId*1024*(2*inRank+3) + (workLen_s[threadId] - 1)*(2*inRank+3);
-			fnCalcFunLimitsStyblinski_CUDA(inBox + bInd, inRank);
+			bInd = threadId*1024*(2*inRank+3 + (workLen_s[threadId] - 1)*(2*inRank+3);
+			fnCalcFunLimitsRozenbroke_CUDA(inBox + bInd, inRank);
 			
 			if(min_s[threadId] > inBox[bInd + 2*inRank + 2])
 			{
