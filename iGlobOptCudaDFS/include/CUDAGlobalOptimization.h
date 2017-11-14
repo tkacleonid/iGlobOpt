c665d90d277a1b17637102404573f2ea4c696341
tkacleonid <tkacleonid@ya.ru>
2017-11-14 04:44:12 +0300

    debug cuda dfs

--- iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
+++ iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
@@ -778,7 +778,7 @@ __global__ void globOptCUDA(double *inBox, int inRank, int *workLen, double *min
 			
 		__syncthreads();
 			
-		if(threadId == 0 && ((n > 200)|| (count[threadId]+1) % 50000 == 0)) // (count[threadId]+1) % 10000 == 0)
+		if(threadId == 0 && ((n > 10000)|| (count[threadId]+1) % 1000000 == 0)) // (count[threadId]+1) % 10000 == 0)
 		{
 			for(i = 0; i < 1024; i++)
 			{
