cd63423a9e22f25b2cd205f5c8d4bd5251b5e51b
Leonid (Leo) <tkacleonid@ya.ru>
2018-02-01 12:08:53 +0300

    Update interval.h

--- iGlobOptCudaDFS/include/interval.h
+++ iGlobOptCudaDFS/include/interval.h
@@ -9,12 +9,12 @@
 
 
 const int BLOCK_SIZE = 1024;
-const int NUM_BLOCKS = 32;
+const int NUM_BLOCKS = 16;
 const int SIZE_BUFFER_PER_THREAD = 1024; 
 const int TYPE_CUDA_OPTIMIZATION = 2;
-const int MAX_GPU_ITER = 5000000;
+const int MAX_GPU_ITER = 500000;
 const int BORDER_BALANCE = 1;
-const int MAX_ITER_BEFORE_BALANCE = 5000;
+const int MAX_ITER_BEFORE_BALANCE = 500;
 
 
 
