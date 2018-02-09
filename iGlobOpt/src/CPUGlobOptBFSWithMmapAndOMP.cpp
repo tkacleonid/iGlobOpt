ee7b0ef70ecd0b7077e1242f79d773f699430aee
Leonid (Leo) <tkacleonid@ya.ru>
2018-02-09 10:01:34 +0300

    Update CPUGlobOptBFSWithMmapAndOMP.cpp

--- iGlobOpt/src/CPUGlobOptBFSWithMmapAndOMP.cpp
+++ iGlobOpt/src/CPUGlobOptBFSWithMmapAndOMP.cpp
@@ -145,7 +145,7 @@ void calcOptValueOnCPUBFSWithMmapAndOMP(const double *_boxes, int _numBoxes, int
 	while(true)
 	{
 		//scanf("%s",tp);
-		gets(tp);
+		std::cin.get();
 		std::cout << "\n\n-------------------------";
 		std::cout << "wc:  " << wc << "\n\n";
 		for(int i = 0; i < numWorkBoxes; i++)
