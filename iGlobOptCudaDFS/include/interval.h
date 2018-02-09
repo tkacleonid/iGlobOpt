124ba71f2ccc4d0d9a44dc737760db8b4959e728
Leonid (Leo) <tkacleonid@ya.ru>
2018-02-09 06:51:58 +0300

    Update interval.h

--- iGlobOptCudaDFS/include/interval.h
+++ iGlobOptCudaDFS/include/interval.h
@@ -14,7 +14,7 @@ const int SIZE_BUFFER_PER_THREAD = 1024;
 const int TYPE_CUDA_OPTIMIZATION = 2;
 const int MAX_GPU_ITER = 50;
 const int BORDER_BALANCE = 1;
-const int MAX_ITER_BEFORE_BALANCE = 500000000;
+const int MAX_ITER_BEFORE_BALANCE = 50;
 
 
 
