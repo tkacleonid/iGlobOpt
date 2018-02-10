026e500c2e50ec6b340763dc3a2aad9d2d9bb1ac
Leonid (Leo) <tkacleonid@ya.ru>
2018-02-10 19:36:17 +0300

    Update iGlobOpt.h

--- iGlobOpt/include/iGlobOpt.h
+++ iGlobOpt/include/iGlobOpt.h
@@ -33,8 +33,8 @@ const std::string MMAP_FILEPATH = "mmaped.bin";
 const int MAX_BOXES_IN_BUFFER = 10000000;
 const int ARRAY_BOUNDS_LENGTH = 3;
 const long long SIZE_BUFFER_FILE = 2000000000L;
-const int PART_BUFFER_TO_FILE = 2;
-const int PART_BUFFER_FROM_FILE = 4;
+const int PART_BUFFER_TO_FILE = 20;
+const int PART_BUFFER_FROM_FILE = 40;
 
 
 //Poisitions in computing box
