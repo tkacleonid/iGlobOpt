bb1943ac308151ddf654d5b0ad62a508ca43ca20
tkacleonid <tkacleonid@ya.ru>
2018-02-20 18:44:21 +0300

    Create Utils

---
+++ utils/src/kernel.cpp
@@ -0,0 +1,46 @@
+
+#include "CUDAGlobalOptimization.h"
+#include "interval.h"
+#include <time.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <exception>
+
+
+
+int main()
+{
+	
+    int inRank = 2;
+
+	double *inBox = new double[inRank*2];
+	double *outBox = new double[inRank*2];
+	double outMin = 0.0;
+	double inEps = 0.1;
+	int status = -1;
+
+
+	for(int i = 0; i < inRank; i++)
+	{
+		inBox[i*2] = -5.0;
+		inBox[i*2+1] = 5.0;
+	}
+
+	std::cout << "start Main\n";
+
+	std::cout << "start \n";
+
+	fnGetOptValueWithCUDA(inBox, inRank, inEps, outBox, &outMin, &status);
+
+	std::cout << "min = " << outMin << "\t";
+
+	//delete [] inBox;
+	//delete [] outBox;
+
+	return 0;
+}
+
+
+
+
