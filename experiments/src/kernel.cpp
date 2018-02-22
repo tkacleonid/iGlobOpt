70cbb4747996a43ddfa564f58385dcf328a7f919
tkacleonid <tkacleonid@ya.ru>
2018-02-22 04:59:50 +0300

    Changing structure of project

--- experiments/src/kernel.cpp
+++ experiments/src/kernel.cpp
@@ -12,10 +12,24 @@
 int main()
 {	
 	
+	srand(time(NULL));
 	
+	double *boxes = new double[2*n*m*dim];
+	int *numberOfBoxes = new int[n];
 	
-	
-	
+	for(int i = 0; i < n; i++)
+	{
+		numberOfBoxes[i] = rand()%(m+1);
+		for(int j = 0; j < numberOfBoxes[i]; j++)
+		{
+			for(int k = 0; k < dim; k++)
+			{
+				boxes[2*i*m*dim + 2*j*dim] = (rand()%(m+1))/(double) n;
+				boxes[2*i*m*dim + 2*j*dim + 1] = (rand()%(m+1))/(double) n;
+			}
+		}
+		
+	}
 	
 	
 
