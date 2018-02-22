17112e2d1ab16adaa8e6a4a4b2ffd8ef359b6c09
tkacleonid <tkacleonid@ya.ru>
2018-02-22 03:44:13 +0300

    Added initializing boxes part

--- utils/src/kernel.cpp
+++ utils/src/kernel.cpp
@@ -6,41 +6,46 @@
 #include <iostream>
 #include <fstream>
 #include <exception>
-
+#include <stdlib.h>
 
 
 int main()
-{
+{	
 	
-    int inRank = 2;
-
-	double *inBox = new double[inRank*2];
-	double *outBox = new double[inRank*2];
-	double outMin = 0.0;
-	double inEps = 0.1;
-	int status = -1;
-
-
-	for(int i = 0; i < inRank; i++)
-	{
-		inBox[i*2] = -5.0;
-		inBox[i*2+1] = 5.0;
-	}
-
-	std::cout << "start Main\n";
-
-	std::cout << "start \n";
-
-	fnGetOptValueWithCUDA(inBox, inRank, inEps, outBox, &outMin, &status);
-
-	std::cout << "min = " << outMin << "\t";
-
-	//delete [] inBox;
-	//delete [] outBox;
+	
+	
+	
+	
+	
+	
 
 	return 0;
 }
 
+
+void balancingOnCPU(int n, int m, int dim)
+{
+	//Initialize random seed
+	srand(time(NULL));
+	
+	double *boxes = new double[2*n*m*dim];
+	int *numberOfBoxes = new int[n];
+	
+	for(int i = 0; i < n; i++)
+	{
+		numberOfBoxes[i] = rand()%(m+1);
+		for(int j = 0; j < numberOfBoxes[i]; j++)
+		{
+			for(int k = 0; k < dim; k++)
+			{
+				boxes[2*i*m*dim + 2*j*dim] = (rand()%(m+1))/(double) n;
+				boxes[2*i*m*dim + 2*j*dim + 1] = (rand()%(m+1))/(double) n;
+			}
+		}
+		
+	}
+	
+}
 
 
 
