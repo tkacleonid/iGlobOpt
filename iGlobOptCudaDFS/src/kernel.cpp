b1367a6c32aace6b74476566cddfb5ad18125797
Leonid (Leo) <tkacleonid@ya.ru>
2018-03-06 23:37:44 +0300

    Update kernel.cpp

--- iGlobOptCudaDFS/src/kernel.cpp
+++ iGlobOptCudaDFS/src/kernel.cpp
@@ -16,7 +16,7 @@ int main()
 
 	double *inBox = new double[inRank*2];
 	double *outBox = new double[inRank*2];
-	double outMin = 0.0;
+	double outMin = 0.0;0
 	double inEps = 0.1;
 	int status = -1;
 
