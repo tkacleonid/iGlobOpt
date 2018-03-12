45d66049b6df9af646cd6f84f1aa96ce674b063e
tkacleonid <tkacleonid@ya.ru>
2018-03-13 01:38:46 +0300

    balancing
    Added sort

--- experiments/src/kernel.cpp
+++ experiments/src/kernel.cpp
@@ -10,13 +10,15 @@
 
 void balancingOnCPU(int n, int m, int dim);
 void balancingOnCPU2(int n, int m, int dim);
+void sortQuickRecursive(int *indexes,int *ar,  const int n);
+void quickSortBase(int *indexes,int *ar, const int l, const int r);
 
 int main()
 {	
 	
 	srand(time(NULL));
 	
-	balancingOnCPU(100, 100, 2);
+	balancingOnCPU_v3(100, 100, 2);
 		
 	return 0;
 }
@@ -60,6 +62,7 @@ void balancingOnCPU(int n, int m, int dim)
 	int boxIndex = 0;
 	int countAverageBoxesPerThreadMore = 0;
 	int plusOne = 0;
+	int stopMore;
 	
 	
 
@@ -99,11 +102,12 @@ void balancingOnCPU(int n, int m, int dim)
 					//memcpy(boxes + i*m*(2*dim+3) + (workLen[i])*(2*dim+3), boxes + j*m*(2*dim+3) + (workLen[j])*(2*dim+3), sizeof(double)*(2*dim+3)*numBoxesWeTake);
 					workLen[i] += numBoxesWeTake;	
 					
-					if(workLen[j] == averageBoxesPerThread + 1)  {
+					if(workLen[j] == averageBoxesPerThread + 1 && countAverageBoxesPerThreadMore > 0)  {
 						countAverageBoxesPerThreadMore--;
 						//if (countAverageBoxesPerThreadMore == 0)
 					}
-					if((workLen[i] == averageBoxesPerThread && countAverageBoxesPerThreadMore == 0) || workLen[i] == averageBoxesPerThread + 1) 
+
+					if((countAverageBoxesPerThreadMore == 0) || workLen[i] == averageBoxesPerThread + 1) 
 					{
 						curThreadWeTakeBoxesIndex = j;
 						break;	
@@ -112,6 +116,55 @@ void balancingOnCPU(int n, int m, int dim)
 						
 			}
 			
+		}
+		if(countAverageBoxesPerThreadMore == 0) {
+			stopMore = i;
+			break;
+		}
+		
+	}
+	
+	for(int i = stopMore; i < n; i++)
+	{
+
+		if(workLen[i] < averageBoxesPerThread+1 && countAverageBoxesPerThreadMore > 0)
+		{
+			
+			for(int j = curThreadWeTakeBoxesIndex; j < n; j++)
+			{
+				//if(workLen[j] == averageBoxesPerThread + 1 && i > j 
+				if(workLen[j] > averageBoxesPerThread+1)  
+				{
+					if	(countAverageBoxesPerThreadMore > 0) plusOne = 1;
+					else plusOne = 0;
+					numBoxesWeTake = (averageBoxesPerThread+plusOne) - workLen[i] <= workLen[j] - (averageBoxesPerThread+plusOne) ? (averageBoxesPerThread+plusOne) - workLen[i] : workLen[j] - (averageBoxesPerThread+plusOne);
+					if(numBoxesWeTake + workLen[i] == averageBoxesPerThread + 1)  {
+						countAverageBoxesPerThreadMore--;
+						//if (countAverageBoxesPerThreadMore == 0)
+					}
+					
+					workLen[j] -= numBoxesWeTake;
+					//memcpy(boxes + i*m*(2*dim+3) + (workLen[i])*(2*dim+3), boxes + j*m*(2*dim+3) + (workLen[j])*(2*dim+3), sizeof(double)*(2*dim+3)*numBoxesWeTake);
+					workLen[i] += numBoxesWeTake;	
+					
+					if(workLen[j] == averageBoxesPerThread + 1 && countAverageBoxesPerThreadMore > 0)  {
+						countAverageBoxesPerThreadMore--;
+						//if (countAverageBoxesPerThreadMore == 0)
+					}
+
+					if((countAverageBoxesPerThreadMore == 0) || workLen[i] == averageBoxesPerThread + 1) 
+					{
+						curThreadWeTakeBoxesIndex = j;
+						break;	
+					}
+				}
+						
+			}
+			
+		}
+		if(countAverageBoxesPerThreadMore == 0) {
+			stopMore = i;
+			break;
 		}
 		
 	}
@@ -158,9 +211,7 @@ void balancingOnCPU(int n, int m, int dim)
 }
 
 
-
-
-void balancingOnCPU2(int n, int m, int dim)
+void balancingOnCPU_v2(int n, int m, int dim)
 {
 	//Initialize random seed
 	srand(time(NULL));
@@ -271,3 +322,165 @@ void balancingOnCPU2(int n, int m, int dim)
 				
 }
 
+
+
+void balancingOnCPU_v3(int n, int m, int dim)
+{
+	//Initialize random seed
+	srand(time(NULL));
+	
+	double *boxes = new double[(2*dim+3) * n*m];
+	int *workLen = new int[n];
+	
+	for(int i = 0; i < n; i++)
+	{
+		workLen[i] = rand()%(m+1);
+		for(int j = 0; j < workLen[i]; j++)
+		{
+			for(int k = 0; k < dim; k++)
+			{
+				boxes[(2*dim+3)*i*m + (2*dim+3)*j + 2*k] = (rand() % (m+1))/(double) n;
+				boxes[(2*dim+3)*i*m + (2*dim+3)*j + 2*k + 1] = (rand() % (m+1))/(double) n;
+			}
+		}		
+	}
+	
+	printf("\n\n");
+	for(int i = 0; i < n; i++)
+	{		
+		printf("%d\t", workLen[i]);	
+	}
+	printf("\n\n");
+	
+	int *workLenIndexes = new workLenIndexes[n];
+	for (int i = 0; i < n; i++) {
+		workLenIndexes[i] = i;
+	}
+	
+	sortQuickRecursive(workLenIndexes,workLen,n);
+	
+				
+				
+/*	
+	int numWorkBoxes = 0;
+	int averageBoxesPerThread = 0;
+	int curThreadWeTakeBoxesIndex = -1;
+	int curThreadWeTakeBoxesCount = 0;
+	int numBoxesWeTake = 0;
+	int boxIndex = 0;
+	int countAverageBoxesPerThreadMore = 0;
+	int plusOne = 0;
+	int i,j;
+	
+
+	for(i = 0; i < n; i++)
+	{
+		numWorkBoxes += workLen[i]; 	
+	}
+	averageBoxesPerThread = numWorkBoxes / n;
+			
+	if(averageBoxesPerThread == 0) averageBoxesPerThread = averageBoxesPerThread + 1;
+			
+	curThreadWeTakeBoxesIndex = 0;
+	for(i = 0; i < n; i++)
+	{
+		if(workLen[i] < averageBoxesPerThread)
+		{
+			for(j = curThreadWeTakeBoxesIndex; j < n; j++)
+			{
+				if(workLen[j] > averageBoxesPerThread)
+				{
+							
+					numBoxesWeTake = averageBoxesPerThread - workLen[i] <= workLen[j] - averageBoxesPerThread ? averageBoxesPerThread - workLen[i] : workLen[j] - averageBoxesPerThread;
+					workLen[j] -= numBoxesWeTake;
+					//memcpy(inBox + (i+blockIdx.x * BLOCK_SIZE)*SIZE_BUFFER_PER_THREAD*(2*inRank+3) + (workLen_s[i])*(2*inRank+3), inBox + (j+blockIdx.x * BLOCK_SIZE)*SIZE_BUFFER_PER_THREAD*(2*inRank+3) + (workLen_s[j])*(2*inRank+3), sizeof(double)*(2*inRank+3)*numBoxesWeTake);
+					workLen[i] += numBoxesWeTake;	
+					if(workLen[i] == averageBoxesPerThread) 
+					{
+						break;	
+					}
+				}
+						
+			}
+			curThreadWeTakeBoxesIndex = j;
+		}
+				
+	}
+			
+			
+	boxIndex = 0;
+	for(i = 0; i < n; i++)
+	{
+		if(workLen[i] == averageBoxesPerThread)
+		{
+			for(j = curThreadWeTakeBoxesIndex; j < n; j++)
+			{
+				if(workLen[j] > averageBoxesPerThread + 1)
+				{
+					numBoxesWeTake = 1;
+					workLen[j] -= numBoxesWeTake;
+					//memcpy(inBox + (i+blockIdx.x * BLOCK_SIZE)*SIZE_BUFFER_PER_THREAD*(2*inRank+3) + (workLen_s[i])*(2*inRank+3), inBox + (j+blockIdx.x * BLOCK_SIZE)*SIZE_BUFFER_PER_THREAD*(2*inRank+3) + (workLen_s[j])*(2*inRank+3), sizeof(double)*(2*inRank+3)*numBoxesWeTake);
+					workLen[i] += numBoxesWeTake;	
+					break;
+				}
+						
+			}
+			curThreadWeTakeBoxesIndex = j;
+		}
+		if(curThreadWeTakeBoxesIndex == n - 1 && workLen[curThreadWeTakeBoxesIndex] <= averageBoxesPerThread + 1)
+		{
+			break;
+		}
+	}
+	*/
+
+	printf("\n\n");
+	for(i = 0; i < n; i++)
+	{		
+		printf("%d\t", workLen[i]);	
+	}
+	printf("\n\n");
+				
+}
+
+
+
+
+
+void sortQuickRecursive(int *indexes,int *ar,  const int n) {
+   quickSortBase(boxes,boxesResult,inRank,0,n-1);
+}
+
+void quickSortBase(int *indexes,int *ar, const int l, const int r) {
+    int i = l, j = r;
+    int pp[3] = { ar[l], ar[r], ar[(l+r)>>1]};
+    int p = pp[0];
+    if (pp[1] >= pp[0] && pp[1]<=pp[0]) p=pp[1];
+    else if (pp[2] >= pp[0] && pp[2]<=pp[1]) p=pp[2];
+    
+    while (i <= j) {
+        while (p > ar[i])
+           i++;
+        while (ar[j] > p)
+           j--;
+        if (i <= j) {
+			int temp;
+			temp = ar[i];
+			ar[i] = ar[j];
+			ar[j] = temp;
+			
+
+			temp = indexes[i];
+			indexes[i] = indexes[j];
+			indexes[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
+
+    if (l < j)
+       quickSortBase(indexes,ar,l, j);
+    if (i < r)
+       quickSortBase(indexes,ar,i, r);
+}
