346bcd44967051e2a7e80a8de027b30b3b851091
tkacleonid <tkacleonid@ya.ru>
2018-03-15 00:48:21 +0300

    balancing

--- experiments/balancing/src/balancing.cpp
+++ experiments/balancing/src/balancing.cpp
@@ -1,6 +1,47 @@
 
 #include "balancing.hpp"
 
+
+
+void sortQuickRecursive(int *indexes,int *ar,  const int n) {
+   quickSortBase(indexes,ar,0,n-1);
+}
+
+void quickSortBase (int *indexes,int *ar, const int l, const int r) {
+    int i = l, j = r;
+    int pp[3] = { ar[l], ar[r], ar[(l+r)>>1]};
+    int p = pp[0];
+    if (pp[1] >= pp[0] && pp[1]<=pp[0]) p=pp[1];
+    else if (pp[2] >= pp[0] && pp[2]<=pp[1]) p=pp[2];
+    
+    while (i <= j) {
+        while (p > ar[i])
+           i++;
+        while (ar[j] > p)
+           j--;
+        if (i <= j) {
+			int temp;
+			temp = ar[i];
+			ar[i] = ar[j];
+			ar[j] = temp;
+			
+
+			temp = indexes[i];
+			indexes[i] = indexes[j];
+			indexes[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
+
+    if (l < j)
+       quickSortBase(indexes,ar,l, j);
+    if (i < r)
+       quickSortBase(indexes,ar,i, r);
+}
+
+
 void initializeBoxes(double* boxes, int *workLen, int n, int m, int dim)
 {
 	//Initialize random seed
@@ -392,46 +433,4 @@ BalancingInfo balancingOnCPU_v3(double* boxes, int *workLen, int n, int m, int d
 	printf("\n\n");
 	printf("countMemoryCopies: %d\n", countMemoryCopies);
 				
-}
-
-
-
-
-
-void sortQuickRecursive(int *indexes,int *ar,  const int n) {
-   quickSortBase(indexes,ar,0,n-1);
-}
-
-void quickSortBase(int *indexes,int *ar, const int l, const int r) {
-    int i = l, j = r;
-    int pp[3] = { ar[l], ar[r], ar[(l+r)>>1]};
-    int p = pp[0];
-    if (pp[1] >= pp[0] && pp[1]<=pp[0]) p=pp[1];
-    else if (pp[2] >= pp[0] && pp[2]<=pp[1]) p=pp[2];
-    
-    while (i <= j) {
-        while (p > ar[i])
-           i++;
-        while (ar[j] > p)
-           j--;
-        if (i <= j) {
-			int temp;
-			temp = ar[i];
-			ar[i] = ar[j];
-			ar[j] = temp;
-			
-
-			temp = indexes[i];
-			indexes[i] = indexes[j];
-			indexes[j] = temp;
-
-            i++;
-            j--;
-        }
-    }
-
-    if (l < j)
-       quickSortBase(indexes,ar,l, j);
-    if (i < r)
-       quickSortBase(indexes,ar,i, r);
 }
