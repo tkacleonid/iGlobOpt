d11b6a214408f04c7c7d722c99225d0cdbf34e17
tkacleonid <tkacleonid@ya.ru>
2018-03-16 23:44:18 +0300

    balancing fixing

--- experiments/balancing/src/balancing.cpp
+++ experiments/balancing/src/balancing.cpp
@@ -350,7 +350,7 @@ BalancingInfo balancingOnCPU_v3(double* boxes, int *workLen, int n, int m, int d
 
 	int numWorkBoxes;
 	int averageBoxesPerThread;
-	int curThreadWeTakeBoxesIndex = -1;
+	int curThreadWeTakeBoxesIndex;
 	int numBoxesWeTake = 0;
 	int countAverageBoxesPerThreadMore = 0;
 	int curThreadWeGiveBoxesIndex = -1;
