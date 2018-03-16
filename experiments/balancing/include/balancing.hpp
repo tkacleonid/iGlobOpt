11daba536901b91737d3341417fb2acd15c25f22
tkacleonid <tkacleonid@ya.ru>
2018-03-17 02:30:10 +0300

    balancing

--- experiments/balancing/include/balancing.hpp
+++ experiments/balancing/include/balancing.hpp
@@ -41,5 +41,6 @@ BalancingInfo balancingOnCPU2(int n, int m, int dim);
 void sortQuickRecursive(int *indexes,int *ar,  const int n);
 void quickSortBase(int *indexes,int *ar, const int l, const int r);
 BalancingInfo balancingOnCPU_v3(double* boxes, int *workLen, int n, int m, int dim);
+BalancingInfo balancingOnCPU_v2(double* boxes, int *workLen, int n, int m, int dim);
 void initializeBoxes(double* boxes, int *workLen, int n, int m, int dim);
 
