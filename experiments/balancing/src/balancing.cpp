3af692797a774751d3c770597021b80cab1f7c65
tkacleonid <tkacleonid@ya.ru>
2018-03-18 23:46:05 +0300

    balancing

--- experiments/balancing/src/balancing.cpp
+++ experiments/balancing/src/balancing.cpp
@@ -155,7 +155,7 @@ BalancingInfo balancingOnCPU_v3(double* boxes, int *workLen, int n, int m, int d
 
 	int numWorkBoxes ;
 	int averageBoxesPerThread ;
-	int curThreadWeTakeBoxesIndex ;
+	int curThreadWeTakeBoxesIndex = 0;
 	int numBoxesWeTake  = 0;
 	int countAverageBoxesPerThreadMore = 0;
 	int curThreadWeGiveBoxesIndex = 0;
