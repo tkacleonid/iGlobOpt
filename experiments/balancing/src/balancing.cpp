4851633e02c2e5608d07ddf35f15c6517a279bda
tkacleonid <tkacleonid@ya.ru>
2018-03-18 23:27:26 +0300

    balancing

--- experiments/balancing/src/balancing.cpp
+++ experiments/balancing/src/balancing.cpp
@@ -145,8 +145,6 @@ BalancingInfo balancingOnCPU_v2(double* boxes, int *workLen, int n, int m, int d
 				
 }
 
-
-
 BalancingInfo balancingOnCPU_v3(double* boxes, int *workLen, int n, int m, int dim)
 {	
 	int *workLenIndexes = new int[n];
@@ -231,6 +229,88 @@ BalancingInfo balancingOnCPU_v3(double* boxes, int *workLen, int n, int m, int d
 			
 	}
 			
+	auto end = std::chrono::high_resolution_clock::now();
+	
+	balancingInfo.time = (std::chrono::duration_cast<std::chrono::microseconds>(end - start)).count();
+	balancingInfo.numberOfMemoryCopies = countMemoryCopies;
+	
+	return balancingInfo;
+				
+}
+
+
+
+BalancingInfo balancingOnGPU_v1(double* boxes, int *workLen, int n, int m, int dim)
+{
+				
+	int numWorkBoxes = ;
+	int averageBoxesPerThread = 0;
+	int curThreadWeTakeBoxesIndex = 0;
+	int numBoxesWeTake = 0;
+	int i,j;
+	int countMemoryCopies = 0;
+	int countAverageBoxesPerThreadMore = 0;
+	
+	BalancingInfo balancingInfo;
+	balancingInfo.numThreads = n;
+	balancingInfo.maxNumberOfBoxesPerThread = m;
+	balancingInfo.version = WITHOUT_SORT_ON_CPU;
+	
+	auto start = std::chrono::high_resolution_clock::now();
+
+	for (i = 0; i < n; i++) {
+		numWorkBoxes += workLen[i]; 	
+	}
+		
+	averageBoxesPerThread = numWorkBoxes / n;	
+	countAverageBoxesPerThreadMore = numWorkBoxes - averageBoxesPerThread*n;
+	if(averageBoxesPerThread == 0) averageBoxesPerThread = averageBoxesPerThread + 1;
+	
+	balancingInfo.numAllBoxes = numWorkBoxes;
+	balancingInfo.numAverageBoxes = countAverageBoxesPerThreadMore;
+	
+		
+	curThreadWeTakeBoxesIndex = 0;
+	countMemoryCopies = 0;
+	for (i = 0; i < n; i++) {
+		if (workLen[i] < averageBoxesPerThread) {
+			for (j = curThreadWeTakeBoxesIndex; j < n; j++) {
+				if (workLen[j] > averageBoxesPerThread) {		
+					numBoxesWeTake = averageBoxesPerThread - workLen[i] <= workLen[j] - averageBoxesPerThread ? averageBoxesPerThread - workLen[i] : workLen[j] - averageBoxesPerThread;
+					workLen[j] -= numBoxesWeTake;
+					memcpy(boxes + i*m*(2*dim+3) + workLen[i]*(2*dim+3), boxes + j*m*(2*dim+3) + workLen[j]*(2*dim+3), sizeof(double)*(2*dim+3)*numBoxesWeTake);
+					workLen[i] += numBoxesWeTake;	
+					countMemoryCopies++;
+					if (workLen[i] == averageBoxesPerThread) break;	
+				}			
+			}
+			curThreadWeTakeBoxesIndex = j;
+		}
+				
+	}
+						
+	
+	for (i = 0; i < n; i++) {
+		if (workLen[i] == averageBoxesPerThread) {
+			for (j = curThreadWeTakeBoxesIndex; j < n; j++) {
+				if (workLen[j] > averageBoxesPerThread + 1) {
+					numBoxesWeTake = 1;
+					workLen[j] -= numBoxesWeTake;
+					memcpy(boxes + i*m*(2*dim+3) + workLen[i]*(2*dim+3), boxes + j*m*(2*dim+3) + workLen[j]*(2*dim+3), sizeof(double)*(2*dim+3)*numBoxesWeTake);
+					workLen[i] += numBoxesWeTake;	
+					countMemoryCopies++;
+					break;
+				}
+						
+			}
+			curThreadWeTakeBoxesIndex = j;
+		}
+		if (curThreadWeTakeBoxesIndex == n - 1 && workLen[curThreadWeTakeBoxesIndex] <= averageBoxesPerThread + 1) break;
+
+	}
+	
+	
+	
 	auto end = std::chrono::high_resolution_clock::now();
 	
 	balancingInfo.time = (std::chrono::duration_cast<std::chrono::microseconds>(end - start)).count();
