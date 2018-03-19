dff8e6c5305e70a27160da5114a26e11da46bf83
tkacleonid <tkacleonid@ya.ru>
2018-03-19 07:36:53 +0300

    balancing

--- experiments/balancing/src/balancing.cpp
+++ experiments/balancing/src/balancing.cpp
@@ -522,7 +522,14 @@ __global__ void balancingCUDA_v1(double *boxes, const int dim, int *workLen, int
 							
 						numBoxesWeTake = averageBoxesPerThread - workLen_s[i] <= workLen_s[j] - averageBoxesPerThread ? averageBoxesPerThread - workLen_s[i] : workLen_s[j] - averageBoxesPerThread;
 						workLen_s[j] -= numBoxesWeTake;
-						memcpy(boxes + (i+blockIdx.x * blockDim.x)*m*(2*dim+3) + (workLen_s[i])*(2*dim+3), boxes + (j+blockIdx.x * blockDim.x)*m*(2*dim+3) + (workLen_s[j])*(2*dim+3), sizeof(double)*(2*dim+3)*numBoxesWeTake);
+						
+						for (int d = 0; d < numBoxesWeTake; d++) {
+							int indTo = (i+blockIdx.x * blockDim.x)*m*(2*dim+3) + (workLen_s[i])*(2*dim+3) + d*(2*dim+3);
+							int indFrom = (j+blockIdx.x * blockDim.x)*m*(2*dim+3) + (workLen_s[j])*(2*dim+3);
+							boxes[indTo] = boxes[indFrom];
+						}
+						
+						//memcpy(boxes + (i+blockIdx.x * blockDim.x)*m*(2*dim+3) + (workLen_s[i])*(2*dim+3), boxes + (j+blockIdx.x * blockDim.x)*m*(2*dim+3) + (workLen_s[j])*(2*dim+3), sizeof(double)*(2*dim+3)*numBoxesWeTake);
 						countMemoryCopies[threadIdx.x] = countMemoryCopies[threadIdx.x] + 1;
 						workLen_s[i] += numBoxesWeTake;	
 						if(workLen_s[i] == averageBoxesPerThread) 
