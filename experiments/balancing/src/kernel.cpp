4ab2cc3b11a89d61aa818dffc7a60422cec58ff0
tkacleonid <tkacleonid@ya.ru>
2018-03-19 13:32:24 +0300

    balancing

--- experiments/balancing/src/kernel.cpp
+++ experiments/balancing/src/kernel.cpp
@@ -8,7 +8,7 @@ int main()
 	int maxBoxesPerThread = 10000;
 
 	
-	testGPUKernelRun(100, dim3(512), dim3(512));
+	testGPUKernelRun(100, dim3(2), dim3(2));
 	
 
 	double *boxes = new double[(2*dim+3) * numThreads*maxBoxesPerThread];
