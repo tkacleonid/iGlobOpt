20c99524e609eac8572681f0d78a2adc5d17687e
tkacleonid <tkacleonid@ya.ru>
2018-03-19 13:41:38 +0300

    balancing

--- experiments/balancing/src/balancing.cpp
+++ experiments/balancing/src/balancing.cpp
@@ -17,7 +17,7 @@ void testGPUKernelRun(const int numRuns, dim3 gridSize, dim3 blockSize)
 	
 	CHECKED_CALL(cudaSetDevice(DEVICE));
 	CHECKED_CALL(cudaDeviceReset());
-	/*
+	
 	auto start = std::chrono::high_resolution_clock::now();
 	for(int i = 0; i < numRuns; i++)
 	{
@@ -27,18 +27,17 @@ void testGPUKernelRun(const int numRuns, dim3 gridSize, dim3 blockSize)
 	
 	printf("AverageTime without synchronize: %d microseconds\n", (std::chrono::duration_cast<std::chrono::microseconds>(end - start)).count()/numRuns);
 
-	*/
-	auto start = std::chrono::high_resolution_clock::now();
+	/* start = std::chrono::high_resolution_clock::now();
 	for(int i = 0; i < numRuns; i++)
 	{
 		CHECKED_CALL(cudaThreadSynchronize());
 		testCUDARun<<<gridSize, blockSize>>>(0);
 		CHECKED_CALL(cudaThreadSynchronize());
 	}
-	auto end = std::chrono::high_resolution_clock::now();
+	end = std::chrono::high_resolution_clock::now();
 	
 	printf("AverageTime with synchronize: %d microseconds\n", (std::chrono::duration_cast<std::chrono::microseconds>(end - start)).count()/numRuns);
-
+	*/
 	
 }
 
