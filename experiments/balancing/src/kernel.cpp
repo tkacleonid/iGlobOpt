fe3459434ade7ac62c0726bba0673121297e2f17
tkacleonid <tkacleonid@ya.ru>
2018-03-20 02:43:46 +0300

    balancing

--- experiments/balancing/src/kernel.cpp
+++ experiments/balancing/src/kernel.cpp
@@ -8,7 +8,7 @@ int main()
 	int maxBoxesPerThread = 10000;
 
 	
-	testGPUTransferDataToDevice(1000, dim3(2), dim3(2));
+	testGPUTransferDataToDevice(1000, dim3(512), dim3(512));
 	
 
 	double *boxes = new double[(2*dim+3) * numThreads*maxBoxesPerThread];
