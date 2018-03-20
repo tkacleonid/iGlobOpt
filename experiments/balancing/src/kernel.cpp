0cea1346ead4cdc24b6abc3c9f2a98489465b46f
tkacleonid <tkacleonid@ya.ru>
2018-03-20 23:16:20 +0300

    balancing

--- experiments/balancing/src/kernel.cpp
+++ experiments/balancing/src/kernel.cpp
@@ -8,7 +8,7 @@ int main()
 	int maxBoxesPerThread = 10000;
 
 	//for (int i = 0; i < 27; i++) {
-		testGPUMemoryAccess(1, dim3(4), dim3(4), "./build/data/transferDataFromDeviceTest.txt", true);
+		testGPUMemoryAccess(1, dim3(4), dim3(4), "./build/data/deviceMemoryAccessTest.txt", true);
 	//}
 	
 	
