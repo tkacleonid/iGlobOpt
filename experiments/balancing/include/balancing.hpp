e8380663070865bbc3490cce9ac574cde2fc6a31
tkacleonid <tkacleonid@ya.ru>
2018-03-20 13:18:44 +0300

    balancing

--- experiments/balancing/include/balancing.hpp
+++ experiments/balancing/include/balancing.hpp
@@ -67,6 +67,15 @@ void testGPUKernelRun(const int numRuns, dim3 gridSize, dim3 blockSize);
 */
 void testGPUTransferDataToDevice(const int numRuns, dim3 gridSize, dim3 blockSize, long long dataVolume, char* fileName, bool isToFile);
 
+/**
+*	Test transfer data from device
+*	@param numRuns the number of cuda testing calls
+*	@param gridSize CUDA grid's size
+*	@param blockSize CUDA block's size
+*	@param dataVolume the volume of data for transering to CUDA
+*/
+void testGPUTransferDataFromDevice(const int numRuns, dim3 gridSize, dim3 blockSize, long long dataVolume, char* fileName, bool isToFile);
+
 
 BalancingInfo balancingOnCPU(double* boxes, int *workLen,int n, int m, int dim);
 BalancingInfo balancingOnCPU2(int n, int m, int dim);
