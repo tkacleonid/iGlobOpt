4cd1f4aa1ed86de17e300f1c5b92184f68357e67
tkacleonid <tkacleonid@ya.ru>
2018-03-21 06:39:08 +0300

    balancing

--- experiments/balancing/src/balancing.cpp
+++ experiments/balancing/src/balancing.cpp
@@ -178,7 +178,7 @@ void testGPUMemoryAccess(const int numRuns, dim3 gridSize, dim3 blockSize, char*
 	auto start = std::chrono::high_resolution_clock::now();
 	CHECKED_CALL(cudaEventRecord(startCuda, 0));	
 	for (int i = 0; i < numRuns; i++) {
-		testCUDAMemoryAccessRunMultiThread_v1<<<gridSize, blockSize>>>(dev_ar1,dev_ar2,partSize);
+		testCUDAMemoryAccessRunSingleThread_v1<<<gridSize, blockSize>>>(dev_ar1,dev_ar2,partSize);
 	}
 	//CHECKED_CALL(cudaGetLastError());
 	CHECKED_CALL(cudaEventRecord(stopCuda, 0));
