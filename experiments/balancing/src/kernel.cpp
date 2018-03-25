1d4082ccc46e5fa6eb0e56f0c7b605de46f13ec7
tkacleonid <tkacleonid@ya.ru>
2018-03-26 00:14:38 +0300

    balancing

--- experiments/balancing/src/kernel.cpp
+++ experiments/balancing/src/kernel.cpp
@@ -15,114 +15,140 @@ int main()
 	
 	BalancingInfo balancingInfo;
 	
-	printf("..........................\n");
-	printf("Stage 1: \n");
-	printf("\tDimension: %d\n",dim);
-	printf("\tNumber of threads: %d\n",numThreads);
-	printf("\tMax number of boxes for thread: %d\n",maxBoxesPerThread);
+	std::ofstream outfile;
+	outfile.open("balancingTime",std::ios_base::app);
+	if (outfile.fail())
+			throw std::ios_base::failure(std::strerror(errno));
+	for (i = 2; i < 4096; i*=2) {
+		maxBoxesPerThread = i;
+		
+		outfile << maxBoxesPerThread << "\t";
+		
+		printf("..........................\n");
+		printf("Stage 1: \n");
+		printf("\tDimension: %d\n",dim);
+		printf("\tNumber of threads: %d\n",numThreads);
+		printf("\tMax number of boxes for thread: %d\n",maxBoxesPerThread);
+		
+		
+		printf("Initializing boxes version 1\n");
+		initializeBoxes_v1(boxes, workLen, numThreads, maxBoxesPerThread, dim);
+		
+		memcpy(tempBoxes,boxes,sizeof(double)*(2*dim+3) * numThreads*maxBoxesPerThread);
+		memcpy(tempWorkLen,workLen,sizeof(int)*numThreads);	
+		printf("Testing balancing on CPU (version 1)\n");
+		balancingInfo = balancingOnCPU_v1(tempBoxes, tempWorkLen, numThreads, maxBoxesPerThread, dim);
+		printf("numberOfMemoryCopies = %d\n",balancingInfo.numberOfMemoryCopies);
+		printf("time = %f microseconds\n",balancingInfo.time);
+		printf("numAllBoxes = %d\n",balancingInfo.numAllBoxes);
+		printf("numAverageBoxes = %d\n",balancingInfo.numAverageBoxes);
+		
+		outfile << balancingInfo.time << "\t";
+		
+		printf("\n\n");
+		for (int i = 0; i < numThreads; i++) {
+			//printf("%d\t", tempWorkLen[i]);
+		}
+		printf("\n\n");
+		
+		
+		memcpy(tempBoxes,boxes,sizeof(double)*(2*dim+3) * numThreads*maxBoxesPerThread);
+		memcpy(tempWorkLen,workLen,sizeof(int)*numThreads);	
+		printf("\nTesting balancing on CPU (version 2)\n");
+		balancingInfo = balancingOnCPU_v2(tempBoxes, tempWorkLen, numThreads, maxBoxesPerThread, dim);
+		printf("numberOfMemoryCopies = %d\n",balancingInfo.numberOfMemoryCopies);
+		printf("time = %f microseconds\n",balancingInfo.time);
+		printf("numAllBoxes = %d\n",balancingInfo.numAllBoxes);
+		printf("numAverageBoxes = %d\n",balancingInfo.numAverageBoxes);
+		
+		outfile << balancingInfo.time << "\t";
+
+		printf("\n\n");
+		
+		memcpy(tempBoxes,boxes,sizeof(double)*(2*dim+3) * numThreads*maxBoxesPerThread);
+		memcpy(tempWorkLen,workLen,sizeof(int)*numThreads);	
+		
+		printf("\nTesting balancing on GPU (version 1)\n");
+		balancingInfo = balancingOnGPU_v1(tempBoxes, tempWorkLen, numThreads, maxBoxesPerThread, dim);
+		printf("numberOfMemoryCopies = %d\n",balancingInfo.numberOfMemoryCopies);
+		printf("time = %f milliseconds\n",balancingInfo.time);
+		printf("numAllBoxes = %d\n",balancingInfo.numAllBoxes);
+		printf("numAverageBoxes = %d\n",balancingInfo.numAverageBoxes);
+		
+		outfile << balancingInfo.time << "\t";
+		
+		printf("\n\n");
+
+		
+		memcpy(tempBoxes,boxes,sizeof(double)*(2*dim+3) * numThreads*maxBoxesPerThread);
+		memcpy(tempWorkLen,workLen,sizeof(int)*numThreads);	
+		
+
+
+		printf("\nTesting balancing on GPU (version 2)\n");
+		balancingInfo = balancingOnGPU_v2(tempBoxes, tempWorkLen, numThreads, maxBoxesPerThread, dim);
+		printf("numberOfMemoryCopies = %d\n",balancingInfo.numberOfMemoryCopies);
+		printf("time = %f milliseconds\n",balancingInfo.time);
+		printf("numAllBoxes = %d\n",balancingInfo.numAllBoxes);
+		printf("numAverageBoxes = %d\n",balancingInfo.numAverageBoxes);
+		
+		outfile << balancingInfo.time << "\t";
+		
+		printf("\n\n");
+
+		
+		
+		printf("..........................\n");
+		
+		
+		
+		printf("..........................\n");
+		printf("Stage 2: \n");
+		printf("\tDimension: %d\n",dim);
+		printf("\tNumber of threads: %d\n",numThreads);
+		printf("\tMax number of boxes for thread: %d\n",maxBoxesPerThread);
+		
+		
+		printf("Initializing boxes version 2\n");
+		initializeBoxes_v2(boxes, workLen, numThreads, maxBoxesPerThread, dim);
+		
+		
+		memcpy(tempBoxes,boxes,sizeof(double)*(2*dim+3) * numThreads*maxBoxesPerThread);
+		memcpy(tempWorkLen,workLen,sizeof(int)*numThreads);	
+
+		
+		printf("Testing balancing on CPU (version 3)\n");
+		balancingInfo = balancingOnCPU_v3(tempBoxes, tempWorkLen, numThreads, maxBoxesPerThread, dim);
+		printf("numberOfMemoryCopies = %d\n",balancingInfo.numberOfMemoryCopies);
+		printf("time = %f microseconds\n",balancingInfo.time);
+		printf("numAllBoxes = %d\n",balancingInfo.numAllBoxes);
+		printf("numAverageBoxes = %d\n",balancingInfo.numAverageBoxes);
+		
+		outfile << balancingInfo.time << "\t";
+		
+		printf("\n\n");
+		
+		
+		memcpy(tempBoxes,boxes,sizeof(double)*(2*dim+3) * numThreads*maxBoxesPerThread);
+		memcpy(tempWorkLen,workLen,sizeof(int)*numThreads);	
+		
+		printf("\nTesting balancing on GPU (version 3)\n");
+		balancingInfo = balancingOnGPU_v3(tempBoxes, tempWorkLen, numThreads, maxBoxesPerThread, dim);
+		printf("numberOfMemoryCopies = %d\n",balancingInfo.numberOfMemoryCopies);
+		printf("time = %f milliseconds\n",balancingInfo.time);
+		printf("numAllBoxes = %d\n",balancingInfo.numAllBoxes);
+		printf("numAverageBoxes = %d\n",balancingInfo.numAverageBoxes);
+		
+		outfile << balancingInfo.time << "\t";
+		
+		printf("\n\n");
 	
-	
-	printf("Initializing boxes version 1\n");
-	initializeBoxes_v1(boxes, workLen, numThreads, maxBoxesPerThread, dim);
-	
-	memcpy(tempBoxes,boxes,sizeof(double)*(2*dim+3) * numThreads*maxBoxesPerThread);
-	memcpy(tempWorkLen,workLen,sizeof(int)*numThreads);	
-	printf("Testing balancing on CPU (version 1)\n");
-	balancingInfo = balancingOnCPU_v1(tempBoxes, tempWorkLen, numThreads, maxBoxesPerThread, dim);
-	printf("numberOfMemoryCopies = %d\n",balancingInfo.numberOfMemoryCopies);
-	printf("time = %f microseconds\n",balancingInfo.time);
-	printf("numAllBoxes = %d\n",balancingInfo.numAllBoxes);
-	printf("numAverageBoxes = %d\n",balancingInfo.numAverageBoxes);
-	
-	printf("\n\n");
-	for (int i = 0; i < numThreads; i++) {
-		//printf("%d\t", tempWorkLen[i]);
+		outfile << "\n"; 
+		
 	}
-	printf("\n\n");
-	
-	
-	memcpy(tempBoxes,boxes,sizeof(double)*(2*dim+3) * numThreads*maxBoxesPerThread);
-	memcpy(tempWorkLen,workLen,sizeof(int)*numThreads);	
-	printf("\nTesting balancing on CPU (version 2)\n");
-	balancingInfo = balancingOnCPU_v2(tempBoxes, tempWorkLen, numThreads, maxBoxesPerThread, dim);
-	printf("numberOfMemoryCopies = %d\n",balancingInfo.numberOfMemoryCopies);
-	printf("time = %f microseconds\n",balancingInfo.time);
-	printf("numAllBoxes = %d\n",balancingInfo.numAllBoxes);
-	printf("numAverageBoxes = %d\n",balancingInfo.numAverageBoxes);
-
-	printf("\n\n");
-	
-	memcpy(tempBoxes,boxes,sizeof(double)*(2*dim+3) * numThreads*maxBoxesPerThread);
-	memcpy(tempWorkLen,workLen,sizeof(int)*numThreads);	
-	
-	printf("\nTesting balancing on GPU (version 1)\n");
-	balancingInfo = balancingOnGPU_v1(tempBoxes, tempWorkLen, numThreads, maxBoxesPerThread, dim);
-	printf("numberOfMemoryCopies = %d\n",balancingInfo.numberOfMemoryCopies);
-	printf("time = %f milliseconds\n",balancingInfo.time);
-	printf("numAllBoxes = %d\n",balancingInfo.numAllBoxes);
-	printf("numAverageBoxes = %d\n",balancingInfo.numAverageBoxes);
-	
-	printf("\n\n");
-
-	
-	memcpy(tempBoxes,boxes,sizeof(double)*(2*dim+3) * numThreads*maxBoxesPerThread);
-	memcpy(tempWorkLen,workLen,sizeof(int)*numThreads);	
 	
 
-
-	printf("\nTesting balancing on GPU (version 2)\n");
-	balancingInfo = balancingOnGPU_v2(tempBoxes, tempWorkLen, numThreads, maxBoxesPerThread, dim);
-	printf("numberOfMemoryCopies = %d\n",balancingInfo.numberOfMemoryCopies);
-	printf("time = %f milliseconds\n",balancingInfo.time);
-	printf("numAllBoxes = %d\n",balancingInfo.numAllBoxes);
-	printf("numAverageBoxes = %d\n",balancingInfo.numAverageBoxes);
-	
-	printf("\n\n");
-
-	
-	
-	printf("..........................\n");
-	
-	
-	
-	printf("..........................\n");
-	printf("Stage 2: \n");
-	printf("\tDimension: %d\n",dim);
-	printf("\tNumber of threads: %d\n",numThreads);
-	printf("\tMax number of boxes for thread: %d\n",maxBoxesPerThread);
-	
-	
-	printf("Initializing boxes version 2\n");
-	initializeBoxes_v2(boxes, workLen, numThreads, maxBoxesPerThread, dim);
-	
-	
-	memcpy(tempBoxes,boxes,sizeof(double)*(2*dim+3) * numThreads*maxBoxesPerThread);
-	memcpy(tempWorkLen,workLen,sizeof(int)*numThreads);	
-
-	
-	printf("Testing balancing on CPU (version 3)\n");
-	balancingInfo = balancingOnCPU_v3(tempBoxes, tempWorkLen, numThreads, maxBoxesPerThread, dim);
-	printf("numberOfMemoryCopies = %d\n",balancingInfo.numberOfMemoryCopies);
-	printf("time = %f microseconds\n",balancingInfo.time);
-	printf("numAllBoxes = %d\n",balancingInfo.numAllBoxes);
-	printf("numAverageBoxes = %d\n",balancingInfo.numAverageBoxes);
-	
-	printf("\n\n");
-	
-	
-	memcpy(tempBoxes,boxes,sizeof(double)*(2*dim+3) * numThreads*maxBoxesPerThread);
-	memcpy(tempWorkLen,workLen,sizeof(int)*numThreads);	
-	
-	printf("\nTesting balancing on GPU (version 3)\n");
-	balancingInfo = balancingOnGPU_v3(tempBoxes, tempWorkLen, numThreads, maxBoxesPerThread, dim);
-	printf("numberOfMemoryCopies = %d\n",balancingInfo.numberOfMemoryCopies);
-	printf("time = %f milliseconds\n",balancingInfo.time);
-	printf("numAllBoxes = %d\n",balancingInfo.numAllBoxes);
-	printf("numAverageBoxes = %d\n",balancingInfo.numAverageBoxes);
-	
-	printf("\n\n");
-
-	
+	outfile.close();
 	
 	delete [] boxes;
 	delete [] workLen;
