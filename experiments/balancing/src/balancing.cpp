17c29c0d0ab43054b048a506c83815aea637afdf
tkacleonid <tkacleonid@ya.ru>
2018-03-26 07:51:05 +0300

    balancing

--- experiments/balancing/src/balancing.cpp
+++ experiments/balancing/src/balancing.cpp
@@ -503,7 +503,10 @@ BalancingInfo balancingOnCPU_v1(double* boxes, int *workLen, int n, int m, int d
 				if (workLen[j] > averageBoxesPerThread + 1) {
 					numBoxesWeTake = 1;
 					workLen[j] -= numBoxesWeTake;
-					memcpy(boxes + i*m*(2*dim+3) + workLen[i]*(2*dim+3), boxes + j*m*(2*dim+3) + workLen[j]*(2*dim+3), sizeof(double)*(2*dim+3)*numBoxesWeTake);
+					for (int d = 0; d < numBoxesWeTake; d++) {
+						boxes[i*m*(2*dim+3) + workLen[i]*(2*dim+3) + d] = boxes[j*m*(2*dim+3) + workLen[j]*(2*dim+3)];
+					}
+					//memcpy(boxes + i*m*(2*dim+3) + workLen[i]*(2*dim+3), boxes + j*m*(2*dim+3) + workLen[j]*(2*dim+3), sizeof(double)*(2*dim+3)*numBoxesWeTake);
 					workLen[i] += numBoxesWeTake;	
 					countMemoryCopies++;
 					break;
