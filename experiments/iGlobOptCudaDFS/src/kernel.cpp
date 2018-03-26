c0389e22604137af3db227b2db5c8dd4ec79b2fc
tkacleonid <tkacleonid@ya.ru>
2018-03-27 01:08:39 +0300

    cuda dfs MAX_GPU_INTER = 1024

--- experiments/iGlobOptCudaDFS/src/kernel.cpp
+++ experiments/iGlobOptCudaDFS/src/kernel.cpp
@@ -31,7 +31,7 @@ int main()
 
 	for (int i = 0; i < 5; i++) {
 		inEps /= 10;
-		fnGetOptValueWithCUDA_v2(inBox, inDim, inEps, outBox, &outMin, &status, funRecord, "testGPU11");
+		fnGetOptValueWithCUDA_v2(inBox, inDim, inEps, outBox, &outMin, &status, funRecord, "testGPU12");
 		std::cout << "min = " << outMin << "\n\n";
 	}
 
