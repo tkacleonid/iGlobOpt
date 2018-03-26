83e80f71dcf8faa01d27f8966c2a4bef7d7ed5ad
tkacleonid <tkacleonid@ya.ru>
2018-03-26 15:35:35 +0300

    cuda dfs

--- experiments/iGlobOptCudaDFS/include/interval.h
+++ experiments/iGlobOptCudaDFS/include/interval.h
@@ -13,7 +13,7 @@ const int BLOCK_SIZE = 1024;
 const int NUM_BLOCKS = 1;
 const int SIZE_BUFFER_PER_THREAD = 1024; 
 const int TYPE_CUDA_OPTIMIZATION = 1;
-const int MAX_GPU_ITER = 1;
+const int MAX_GPU_ITER = 1000000;
 const int BORDER_BALANCE = 1;
 const int MAX_ITER_BEFORE_BALANCE = 1;
 
