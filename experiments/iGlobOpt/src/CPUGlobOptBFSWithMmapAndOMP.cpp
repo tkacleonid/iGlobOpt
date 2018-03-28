0bb2bbafd05bf5eded8ca70938f0ce87af1209a5
tkacleonid <tkacleonid@ya.ru>
2018-03-28 23:55:27 +0300

    cpu bfs

--- experiments/iGlobOpt/src/CPUGlobOptBFSWithMmapAndOMP.cpp
+++ experiments/iGlobOpt/src/CPUGlobOptBFSWithMmapAndOMP.cpp
@@ -107,8 +107,7 @@ void calcOptValueOnCPUBFSWithMmapAndOMP(const double *_boxes, int _numBoxes, int
 		}
 	}
 	
-	for(int n = 0; n < numWorkBoxes*1024; n++)
-	{
+	for (int n = 0; n < numWorkBoxes*1024; n++) {
 		for(int i = 0; i < _dim; i++)
 		{
 			if(i == hInd1)
