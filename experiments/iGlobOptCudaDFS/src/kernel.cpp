da0661ea9cbb6c1bee8c76de5a281ec91100786b
tkacleonid <tkacleonid@ya.ru>
2018-03-30 23:39:20 +0300

    test

--- experiments/iGlobOptCudaDFS/src/kernel.cpp
+++ experiments/iGlobOptCudaDFS/src/kernel.cpp
@@ -31,7 +31,7 @@ int main()
 
 	for (int i = 0; i < 5; i++) {
 		inEps /= 10;
-		fnGetOptValueWithCUDA_v3(inBox, inDim, inEps, outBox, &outMin, &status, funRecord, "realWorkingThreadsNumber");
+		fnGetOptValueWithCUDA_v3(inBox, inDim, inEps, outBox, &outMin, &status, funRecord, "realWorkingThreadsNumbe");
 		std::cout << "min = " << outMin << "\n\n";
 	}
 
