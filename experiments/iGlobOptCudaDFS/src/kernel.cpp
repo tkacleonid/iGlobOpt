f1a06f633862a9b52c876163839dbf732038bf9e
tkacleonid <tkacleonid@ya.ru>
2018-03-30 23:40:49 +0300

    test

--- experiments/iGlobOptCudaDFS/src/kernel.cpp
+++ experiments/iGlobOptCudaDFS/src/kernel.cpp
@@ -31,7 +31,7 @@ int main()
 
 	for (int i = 0; i < 5; i++) {
 		inEps /= 10;
-		fnGetOptValueWithCUDA_v3(inBox, inDim, inEps, outBox, &outMin, &status, funRecord, "gpu2");
+		fnGetOptValueWithCUDA_v3(inBox, inDim, inEps, outBox, &outMin, &status, funRecord, "gpu3");
 		std::cout << "min = " << outMin << "\n\n";
 	}
 
