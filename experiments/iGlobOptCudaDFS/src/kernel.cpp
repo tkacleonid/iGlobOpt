2a8ea3aa53ea56f2b5eeace7ad44f57756a2bb47
tkacleonid <tkacleonid@ya.ru>
2018-04-03 00:32:51 +0300

    testing cuda dfs

--- experiments/iGlobOptCudaDFS/src/kernel.cpp
+++ experiments/iGlobOptCudaDFS/src/kernel.cpp
@@ -29,7 +29,7 @@ int main()
 	std::cout << "start Main\n";
 	std::cout << "start \n";
 
-	for (int i = 0; i < 6; i++) {
+	for (int i = 0; i < 5; i++) {
 		inEps /= 10;
 		fnGetOptValueWithCUDA_v2(inBox, inDim, inEps, outBox, &outMin, &status, funRecord, "testGPU21");
 		std::cout << "min = " << outMin << "\n\n";
