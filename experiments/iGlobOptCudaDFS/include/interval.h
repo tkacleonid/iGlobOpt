b1bf5089d7d62a4b329186bf55f4f192ff96a3fb
tkacleonid <tkacleonid@ya.ru>
2018-04-03 00:46:26 +0300

    testing cuda dfs

--- experiments/iGlobOptCudaDFS/include/interval.h
+++ experiments/iGlobOptCudaDFS/include/interval.h
@@ -12,7 +12,7 @@
 
 
 const int BLOCK_SIZE = 512;
-const int NUM_BLOCKS = 8;
+const int NUM_BLOCKS = 16;
 const int SIZE_BUFFER_PER_THREAD = 1024; 
 const int TYPE_CUDA_OPTIMIZATION = 1;
 const int MAX_GPU_ITER = 16384;
