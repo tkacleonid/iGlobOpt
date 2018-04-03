88592618abe9ec16a8beb5b3cc79247dcbbcbca3
tkacleonid <tkacleonid@ya.ru>
2018-04-03 13:14:47 +0300

    testing cuda dfs

--- experiments/iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
+++ experiments/iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
@@ -442,7 +442,7 @@ void fnGetOptValueWithCUDA_v2(double *inBox, const int inDim, const double inEps
 		CHECKED_CALL(cudaEventRecord(start, 0));
 		
 		globOptCUDA_v1<<<GridSize, BLOCK_SIZE>>>(dev_inBox,inDim,dev_workLen,dev_mins,funcMin,inEps,dev_workCounts);
-		printf("-");
+		std::cout << "-";
 		CHECKED_CALL(cudaGetLastError());
 		//printf("-");
 		CHECKED_CALL(cudaEventRecord(stop, 0));
