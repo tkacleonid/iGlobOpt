c6b19e01951c3fedc0d58cd0d229eb6960a14f8f
tkacleonid <tkacleonid@ya.ru>
2018-04-03 12:32:01 +0300

    testing cuda dfs

--- experiments/iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
+++ experiments/iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
@@ -222,7 +222,7 @@ __global__ void globOptCUDA_v1(double *inBox,  int inDim, int *workLen, double *
 			
 			
 			if (hInd == inDim) hInd = 0;
-			h = inBox[bInd + 1 + hInd*2] - inBox[bInd + hInd*2];
+			h = inBox[bInd + hInd*2 + 1] - inBox[bInd + hInd*2];
 			++hInd;
 			
 			/*
