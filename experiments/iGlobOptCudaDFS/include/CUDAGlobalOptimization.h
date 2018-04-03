f5ef6da5f021311d39242bcaa03c74a552ffc105
tkacleonid <tkacleonid@ya.ru>
2018-04-03 13:28:38 +0300

    testing cuda dfs

--- experiments/iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
+++ experiments/iGlobOptCudaDFS/include/CUDAGlobalOptimization.h
@@ -205,7 +205,7 @@ __global__ void globOptCUDA_v1(double *inBox,  int inDim, int *workLen, double *
 		if (min_s[threadIdx.x] > inBox[bInd + 2*inDim + 2]) {
 			min_s[threadIdx.x] = inBox[bInd + 2*inDim + 2];
 		}		
-		if (min_s[threadIdx.x] - inBox[bInd + 2*inDim] < inEps) {
+		while (min_s[threadIdx.x] - inBox[bInd + 2*inDim] < inEps) {
 			--workLen_s[threadIdx.x];
 			bInd = threadId*SIZE_BUFFER_PER_THREAD*(2*inDim+3) + (workLen_s[threadIdx.x] - 1)*(2*inDim+3);
 			n++;
